22c01eee2362d16414e91593e3dd64dccfcc86d6
Santiago Bello Moreno <167466575+SBellom21@users.noreply.github.com>
2025-11-16 18:37:26 -0500

    Update urls.py

--- Proyecto/backend/smartcar_app/urls.py
+++ Proyecto/backend/smartcar_app/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.health),
-    path("usuarios/crear/", views.crear_usuario),  # <-- aquí agregamos /crear/
-    path("usuarios/<int:user_id>/", views.obtener_usuario),
-    path("login/", views.login),
-    path("hola_mundo/", views.hola_mundo),
+    path("health/", views.health, name="health"),
+    path("usuarios/crear/", views.crear_usuario, name="crear_usuario"),
+    path("usuarios/<int:user_id>/", views.obtener_usuario, name="obtener_usuario"),
+    path("login/", views.login, name="login"),
+    path("hola/", views.hola_mundo, name="hola_mundo"),
 ]
+
     
